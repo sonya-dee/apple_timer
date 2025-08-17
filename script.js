9d415d449b5c272ef6cc42005b7ab57d9ec2682c
sonya-dee <sonya.dee15@gmail.com>
2025-08-16 23:58:47 -0500

    bug fixes + additions; incorrect file naming, changed btn-start to start-btn, added pause and clear js functionalities, uploaded apple images to replace buttons, resolved css styling issues

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const bloop = new Audio("./sounds/bloop.mp4");
-const startBtn = document.querySelector('.btn-start');
+const bloop = new Audio("./sounds/bloop.m4a");
+const startBtn = document.querySelector('.start-btn');
 const pauseBtn = document.querySelector('.pause-btn');
 const clearBtn = document.querySelector('.clear-btn');
+const session = document.querySelector('.minutes'); 
+
 let myInterval;
 let state = true;
 
-const appleTimer = () => {
-    const sessionAmount = Number.parseInt(sessionStorage.textContent)
-    if(state) {
-        state = false;
-        let totalSeconds = sessionAmount * 60;
 
-        const updateSeconds = () => {
-            // Fnction 
-        }
-        myInterval = setInterval(updateSeconds, 1000);
-    } 
-}
+ const appleTimer = () => {
+  const sessionAmount = Number.parseInt(session.textContent)
 
-const updateSeconds = () => {
-    const minuteDiv = document.querySelector('.minutes');
-    const secondDiv = document.querySelector('.seconds');
+  if(state) {
+    state = false;
+    let totalSeconds = sessionAmount * 60;
+
+    const updateSeconds = () => {
+       const minuteDiv = document.querySelector('.minutes');
+       const secondDiv = document.querySelector('.seconds');
 
     totalSeconds--;
 
     let minutesLeft = Math.floor(totalSeconds/60);
     let secondsLeft = totalSeconds % 60;
 
-    if(secondsLeft < 10) {
+    if (secondsLeft < 10) {
         secondDiv.textContent = '0' + secondsLeft;
     } else {
         secondDiv.textContent = secondsLeft;
-    } 
-    minuteDiv.textContent = '${minutesLeft}'
+    }
+    minuteDiv.textContent = `${minutesLeft}`
 
     if(minutesLeft === 0 && secondsLeft === 0) {
         bloop.play()
         clearInterval(myInterval);
     }
+} 
+    myInterval = setInterval(updateSeconds, 1000);
+    } else {
+    alert('Session has already started.')
+   }
 }
 
 startBtn.addEventListener('click', appleTimer);
+
+pauseBtn.addEventListener('click', () => {
+    clearInterval(myInterval);
+    state = true;
+});
+
+clearBtn.addEventListener('click', () => {
+    clearInterval(myInterval);
+    document.querySelector('.minutes').textContent = '25'; 
+    document.querySelector('.seconds').textContent = '00';
+    state = true;
+});
